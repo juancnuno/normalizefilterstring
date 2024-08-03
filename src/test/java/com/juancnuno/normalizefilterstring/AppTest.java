53de9d5190a322296cab63a6fc8ecdffd2f21279
Juan C Nuno <juancnuno@gmail.com>
2024-08-03 12:45:43 -0700

    Commit normalizefilterstring

---
+++ src/test/java/com/juancnuno/normalizefilterstring/AppTest.java
@@ -0,0 +1,20 @@
+package com.juancnuno.normalizefilterstring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
