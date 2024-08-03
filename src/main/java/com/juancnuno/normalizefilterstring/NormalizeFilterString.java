09518385a82a740b381c2da7031ecf7cb3c63ed0
Juan C Nuno <juancnuno@gmail.com>
2024-08-03 13:03:42 -0700

    Add NormalizeFilterString

---
+++ src/main/java/com/juancnuno/normalizefilterstring/NormalizeFilterString.java
@@ -0,0 +1,11 @@
+package com.juancnuno.normalizefilterstring;
+
+final class NormalizeFilterString {
+
+    private NormalizeFilterString() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
