f7711da8eb4df38b70ed017177ad5d1eca386585
Juan C Nuno <juancnuno@gmail.com>
2024-08-03 14:08:52 -0700

    Implement NormalizeFilterString

--- src/main/java/com/juancnuno/normalizefilterstring/NormalizeFilterString.java
+++ src/main/java/com/juancnuno/normalizefilterstring/NormalizeFilterString.java
@@ -6,6 +6,6 @@ final class NormalizeFilterString {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(args[0].toLowerCase().replaceAll("[^_0-9a-z]+", " "));
     }
 }
