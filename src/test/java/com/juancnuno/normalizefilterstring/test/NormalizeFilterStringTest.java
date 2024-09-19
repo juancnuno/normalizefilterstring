f9fee7691a4e2466f1d0148c54c3617f3458b556
Juan C Nuno <juancnuno@gmail.com>
2024-09-18 20:32:26 -0700

    Add NormalizeFilterStringTest

---
+++ src/test/java/com/juancnuno/normalizefilterstring/test/NormalizeFilterStringTest.java
@@ -0,0 +1,10 @@
+package com.juancnuno.normalizefilterstring.test;
+
+import org.junit.jupiter.api.Test;
+
+public final class NormalizeFilterStringTest {
+
+    @Test
+    public void foo() {
+    }
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,4 @@
+module com.juancnuno.normalizefilterstring.test {
+    requires org.junit.jupiter.api;
+    exports com.juancnuno.normalizefilterstring.test;
+}
